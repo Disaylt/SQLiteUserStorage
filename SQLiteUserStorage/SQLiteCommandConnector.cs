681d819e9994dd9f426e2a5c1a83b516b3c6661e
Disaylt <disofban@gmail.com>
2022-03-21 17:30:43 +0300

    Добавьте файлы проекта.

---
+++ SQLiteUserStorage/SQLiteCommandConnector.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteUserStorage
+{
+    public static class SQLiteCommandConnector
+    {
+        private const string _connectionPath = "Data Source=usersdata.db";
+        public delegate void ExceptionPusher(Exception ex);
+        public static event ExceptionPusher? PushException;
+        public static void CrateTable(string tableName, params string[] columns)
+        {
+            using var connection = new SqliteConnection(_connectionPath);
+            try
+            {
+                connection.Open();
+                string commandText = SqlTableCreator.GetCommand(tableName, columns);
+                SqliteCommand command = new SqliteCommand(commandText, connection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if(PushException != null)
+                {
+                    PushException.Invoke(ex);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteUserStorage/SqlTableCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteUserStorage
+{
+    internal static class SqlTableCreator
+    {
+        internal static string GetCommand(string tableName, string[] columns)
+        {
+            string createCommand = "CREATE TABLE";
+            string allColumns = string.Empty;
+            foreach (string column in columns)
+            {
+                allColumns += $"{column}, ";
+            }
+            return $"{createCommand} {tableName}({allColumns})";
+        }
+    }
+}
